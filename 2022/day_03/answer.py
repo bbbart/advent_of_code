cfc06aed9482b6d075a916b40eccdc51a2d173b0
Bart Van Loon <bbb@bbbart.be>
2022-12-11 20:47:58 +0100

    day 3

---
+++ 2022/day_03/answer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from string import ascii_lowercase, ascii_uppercase
+
+priority_order = ascii_lowercase + ascii_uppercase
+
+
+def get_priority(letter):
+    return priority_order.index(letter) + 1
+
+
+def p1(data):
+    total_priority = 0
+    for rucksack in data:
+        comp1 = set(rucksack[0 : len(rucksack) // 2])
+        comp2 = set(rucksack[len(rucksack) // 2 :])
+        common = comp1.intersection(comp2).pop()
+        total_priority += get_priority(common)
+
+    return total_priority
+
+
+def p2(data):
+    total_badge_priority = 0
+    for group in zip(*[iter(data)] * 3):
+        rucksack1 = set(group[0])
+        rucksack2 = set(group[1])
+        rucksack3 = set(group[2])
+        badge = rucksack1.intersection(rucksack2).intersection(rucksack3).pop()
+        total_badge_priority += get_priority(badge)
+
+    return total_badge_priority
