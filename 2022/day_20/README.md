a1513102beabea93ea83195ffce8fcb3435fe97e
Bart Van Loon <bbb@bbbart.be>
2022-12-20 12:11:03 +0100

    day 20

---
+++ 2022/day_20/README.md
@@ -0,0 +1,5 @@
+The answer to *p2* produced by this code was not accepted as correct by AoC.
+However, I really can't find my mistake, and it does produce the correct answer (and correct subresults) for the sample input.
+So, I'm still committing it. :-)
+
+Remark: on my system, using pypy3 7.3.10, this code runs in about 0.28 seconds, while in regular python 3.10.8 it takes more than 9 seconds!
---
+++ 2022/day_20/answer.py
@@ -0,0 +1,57 @@
+from copy import deepcopy
+from dataclasses import dataclass
+
+KEY = 811589153
+
+
+@dataclass
+class Number:
+    number: int
+
+    def __post_init__(self):
+        self.decrypted_number = self.number * KEY
+
+
+def p1(data, is_sample):
+    numbers = list(map(Number, map(int, data)))
+
+    for number in deepcopy(numbers):
+        oldpos = numbers.index(number)
+        newpos = (oldpos + number.number) % (len(numbers) - 1)
+        del numbers[oldpos]
+        numbers.insert(newpos, number)
+
+    numbers = [number.number for number in numbers]
+    pos_zero = numbers.index(0)
+    coordinates = [
+        numbers[(pos_zero + delta) % len(numbers)]
+        for delta in (1000, 2000, 3000)
+    ]
+    return sum(coordinates)
+
+
+def p2(data, is_sample):
+
+    numbers_orig = list(map(Number, map(int, data)))
+    numbers_mixed = deepcopy(numbers_orig)
+
+    def mix(numbers_orig, numbers_mixed):
+        for number in numbers_orig:
+            oldpos = numbers_mixed.index(number)
+            newpos = (oldpos + number.decrypted_number) % (
+                len(numbers_mixed) - 1
+            )
+            del numbers_mixed[oldpos]
+            numbers_mixed.insert(newpos, number)
+        return numbers_mixed
+
+    for _ in range(10):
+        numbers_mixed = mix(numbers_orig, numbers_mixed)
+
+    numbers = [number.number for number in numbers_mixed]
+    pos_zero = numbers.index(0)
+    coordinates = [
+        numbers[(pos_zero + delta) % len(numbers)]
+        for delta in (1000, 2000, 3000)
+    ]
+    return sum(coordinates) * KEY
