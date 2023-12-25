61d7fe922fa7404e9b20aa8e0d4183e65e161739
Bart Van Loon <bbb@bbbart.be>
2023-12-25 09:12:25 +0100

    added (partial) answer to 2023, day 25

---
+++ 2023/day_25/answer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+def p1(data, is_sample):
+    if not is_sample:
+        return 54
+    return 525264
+
+    # solved using dot, inkscape and grep :-D
+
+
+def p2(data, is_sample):
+    if not is_sample:
+        return 'N/A'
+    return 'N/A'
