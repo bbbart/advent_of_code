8fa1ca7d9750cf7e5a84bc5ad572c91e3723fab9
Bart Van Loon <bbb@bbbart.be>
2024-12-03 10:15:34 +0100

    cleaner version of 2024 day 3 logic

--- 2024/day_03/answer.py
+++ 2024/day_03/answer.py
@@ -3,29 +3,28 @@
 import re
 from functools import reduce
 
+
 def p1(data: list[str], is_sample: bool):
-    memory = ''.join(data)
-    multiply_instruction = re.compile(r'mul\((?P<fac1>\d+),(?P<fac2>\d+)\)')
+    memory = "".join(data)
+    multiply_instruction = re.compile(r"mul\((?P<fac1>\d+),(?P<fac2>\d+)\)")
 
     total = 0
     for instruction in multiply_instruction.finditer(memory):
-        total += reduce(lambda x, y: x*y, map(int, instruction.groups()))
+        total += reduce(lambda x, y: x * y, map(int, instruction.groups()))
 
     return total
 
-def p2(data: list[str], is_sample: bool):
-    memory_corrupt = ''.join(data).split('don\'t()')
-    memory = memory_corrupt.pop(0)
-    for mem in memory_corrupt:
-        try:
-            memory += mem.split('do()', 1)[1]
-        except IndexError:
-            pass
 
-    multiply_instruction = re.compile(r'mul\((?P<fac1>\d+),(?P<fac2>\d+)\)')
+def p2(data: list[str], is_sample: bool):
+    memory_corrupt = "".join(data).split("do()")
+    memory = ""
+    for mem in memory_corrupt:
+        memory += mem.split("don't()")[0]
+
+    multiply_instruction = re.compile(r"mul\((?P<fac1>\d+),(?P<fac2>\d+)\)")
 
     total = 0
     for instruction in multiply_instruction.finditer(memory):
-        total += reduce(lambda x, y: x*y, map(int, instruction.groups()))
+        total += reduce(lambda x, y: x * y, map(int, instruction.groups()))
 
     return total
