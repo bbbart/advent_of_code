b36f4e997b706776837db9ee00d0994b3a47b105
Bart Van Loon <bbb@bbbart.be>
2024-12-08 23:09:41 +0100

    added 2024, day 7

---
+++ 2024/day_07/answer.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+from itertools import accumulate, product
+from operator import add, mul
+
+
+def p1(data: list[str], is_sample: bool):
+    OPERATORS = (add, mul)
+
+    calib_total = 0
+    for line in data:
+        result, values = line.split(": ")
+        result = int(result)
+        values = tuple(map(int, values.split(" ")))
+
+        for ops in product(OPERATORS, repeat=len(values) - 1):
+            ops = iter(ops)
+
+            def op(x, y):
+                nonlocal ops
+                operator = next(ops)  # pylint: disable=cell-var-from-loop
+                return operator(x, y)
+
+            total = 0
+            for total in accumulate(values, op):
+                if total > result:
+                    break
+            else:
+                if total == result:
+                    calib_total += result
+                    break
+
+    return calib_total
+
+
+def p2(data: list[str], is_sample: bool):
+    OPERATORS = (add, mul, lambda x, y: int(f"{x}{y}"))
+
+    calib_total = 0
+    for line in data:
+        result, values = line.split(": ")
+        result = int(result)
+        values = tuple(map(int, values.split(" ")))
+
+        for ops in product(OPERATORS, repeat=len(values) - 1):
+            ops = iter(ops)
+
+            def op(x, y):
+                nonlocal ops
+                operator = next(ops)  # pylint: disable=cell-var-from-loop
+                return operator(x, y)
+
+            total = 0
+            for total in accumulate(values, op):
+                if total > result:
+                    break
+            else:
+                if total == result:
+                    calib_total += result
+                    break
+
+    return calib_total
