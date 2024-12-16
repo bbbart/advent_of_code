c28d0df8afcf3bef74396763ac0a7abaf8967990
Bart Van Loon <bbb@bbbart.be>
2024-12-16 07:33:55 +0400

    added 2024, day 12 (part two) - based off on https://www.youtube.com/watch?v=fwEUDQBTPKM

--- 2024/day_12/answer.py
+++ 2024/day_12/answer.py
@@ -38,62 +38,181 @@ def p1(data: list[str], is_sample: bool):
     return price
 
 
+# pylint: disable=too-many-branches,too-many-statements
 def p2(data: list[str], is_sample: bool):
-    if not is_sample:
-        return "N/A"
-    return "N/A"
+    # this is a naive implementation of the solution discussed in
+    # https://www.youtube.com/watch?v=fwEUDQBTPKM
 
-    # # 1/ find shapes as before (shape = set of coordinates)
-    # farm: dict[tuple[int], chr] = {}
-    # for y, line in enumerate(data):
-    #     for x, char in enumerate(line):
-    #         farm[(x, y)] = char
+    # 1/ find shapes as before (shape = set of coordinates)
+    farm: dict[tuple[int], chr] = {}
+    for y, line in enumerate(data):
+        for x, char in enumerate(line):
+            farm[(x, y)] = char
 
-    # seen: set[tuple[int]] = set()
-    # fields: list[set[tuple[int]]] = []
+    seen: set[tuple[int]] = set()
+    fields: list[set[tuple[int]]] = []
 
-    # steps = [(-1, 0), (0, -1), (1, 0), (0, 1)]
-    # while seen != set(farm.keys()):
-    #     start = (set(farm.keys()) - seen).pop()
-    #     crop = farm[start]
+    steps = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+    while seen != set(farm.keys()):
+        start = (set(farm.keys()) - seen).pop()
+        crop = farm[start]
 
-    #     to_explore: set[tuple[int]] = set()
-    #     to_explore.add(start)
+        to_explore: set[tuple[int]] = set()
+        to_explore.add(start)
 
-    #     field = set()
-    #     while to_explore:
-    #         x, y = to_explore.pop()
-    #         seen.add((x, y))
-    #         field.add((x, y))
-    #         for dx, dy in steps:
-    #             if (x + dx, y + dy) in field:
-    #                 continue
-    #             if farm.get((x + dx, y + dy), None) == crop:
-    #                 to_explore.add((x + dx, y + dy))
-    #     fields.append(field)
+        field = set()
+        while to_explore:
+            x, y = to_explore.pop()
+            seen.add((x, y))
+            field.add((x, y))
+            for dx, dy in steps:
+                if (x + dx, y + dy) in field:
+                    continue
+                if farm.get((x + dx, y + dy), None) == crop:
+                    to_explore.add((x + dx, y + dy))
+        fields.append(field)
 
-    # # 2/ traverse the boundary clockwise and count the corners
-    # # the code below doesn't make sense
-    # price = 0
-    # for field in fields:
-    #     boundary = {
-    #         cell
-    #         for cell in field
-    #         if any(
-    #             (cell[0] + step[0], cell[1] + step[1]) not in field
-    #             for step in steps
-    #         )
-    #     }
+    # 2/ find the number of edges of each field
+    price = 0
+    for field in fields:
+        # scan up
+        edges_north = 0
+        scanned_up = set()
+        for x, y in field:
+            # find nearest edge upwards
+            if (x, y) in scanned_up:
+                continue
+            scanned_up.add((x, y))
+            while True:
+                y -= 1
+                if (x, y) in field:
+                    if (x, y) in scanned_up:
+                        at_edge = False
+                        break
+                    scanned_up.add((x, y))
+                else:
+                    at_edge = True
+                    edges_north += 1
+                    y += 1
+                    break
+            while at_edge:
+                x -= 1
+                if (x, y) in field and (x, y - 1) not in field:
+                    scanned_up.add((x, y))
+                else:
+                    x += 1
+                    break
+            while at_edge:
+                x += 1
+                if (x, y) in field and (x, y - 1) not in field:
+                    scanned_up.add((x, y))
+                else:
+                    x -= 1
+                    break
+        # scan down
+        edges_south = 0
+        scanned_down = set()
+        for x, y in field:
+            # find nearest edge downwards
+            if (x, y) in scanned_down:
+                continue
+            scanned_down.add((x, y))
+            while True:
+                y += 1
+                if (x, y) in field:
+                    if (x, y) in scanned_down:
+                        at_edge = False
+                        break
+                    scanned_down.add((x, y))
+                else:
+                    at_edge = True
+                    edges_south += 1
+                    y -= 1
+                    break
+            while at_edge:
+                x -= 1
+                if (x, y) in field and (x, y + 1) not in field:
+                    scanned_down.add((x, y))
+                else:
+                    x += 1
+                    break
+            while at_edge:
+                x += 1
+                if (x, y) in field and (x, y + 1) not in field:
+                    scanned_down.add((x, y))
+                else:
+                    x -= 1
+                    break
+        # scan left
+        edges_west = 0
+        scanned_left = set()
+        for x, y in field:
+            # find nearest edge leftwards
+            if (x, y) in scanned_left:
+                continue
+            scanned_left.add((x, y))
+            while True:
+                x -= 1
+                if (x, y) in field:
+                    if (x, y) in scanned_left:
+                        at_edge = False
+                        break
+                    scanned_left.add((x, y))
+                else:
+                    at_edge = True
+                    edges_west += 1
+                    x += 1
+                    break
+            while at_edge:
+                y -= 1
+                if (x, y) in field and (x - 1, y) not in field:
+                    scanned_left.add((x, y))
+                else:
+                    y += 1
+                    break
+            while at_edge:
+                y += 1
+                if (x, y) in field and (x - 1, y) not in field:
+                    scanned_left.add((x, y))
+                else:
+                    y -= 1
+                    break
+        # scan right
+        edges_east = 0
+        scanned_right = set()
+        for x, y in field:
+            # find nearest edge rightwards
+            if (x, y) in scanned_right:
+                continue
+            scanned_right.add((x, y))
+            while True:
+                x += 1
+                if (x, y) in field:
+                    if (x, y) in scanned_right:
+                        at_edge = False
+                        break
+                    scanned_right.add((x, y))
+                else:
+                    at_edge = True
+                    edges_east += 1
+                    x -= 1
+                    break
+            while at_edge:
+                y -= 1
+                if (x, y) in field and (x + 1, y) not in field:
+                    scanned_right.add((x, y))
+                else:
+                    y += 1
+                    break
+            while at_edge:
+                y += 1
+                if (x, y) in field and (x + 1, y) not in field:
+                    scanned_right.add((x, y))
+                else:
+                    y -= 1
+                    break
+        price += len(field) * (
+            edges_north + edges_south + edges_west + edges_east
+        )
 
-    #     edges = 0
-
-    #     for x, y in boundary:
-    #         for step in steps:
-    #             nx, ny = x + step[0], y + step[1]
-    #             if (nx, ny) not in boundary:
-    #                 edges += 1
-
-    #     print(f"area: {len(field)}, sides: {edges}")
-    #     price += len(field) * edges
-
-    # return price
+    return price
